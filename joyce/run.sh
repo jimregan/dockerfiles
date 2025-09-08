d453bbecb6300f055d28853813977febd497c9f4
Jim O'Regan <joregan@kth.se>
2025-09-08 11:48:15 +0000

    trim

--- joyce/run.sh
+++ joyce/run.sh
@@ -1,9 +1,5 @@
-REAL_DISPLAY=$MAC:0
-
-docker run --rm -it --net=host \
-  -e DISPLAY="$REAL_DISPLAY" \
-  -e XAUTHORITY="${XAUTHORITY:-$HOME/.Xauthority}" \
-  -v "${XAUTHORITY:-$HOME/.Xauthority}:${XAUTHORITY:-$HOME/.Xauthority}:ro" \
+docker run -it \
+  -e DISPLAY="$DISPLAY" \
   -e XDG_RUNTIME_DIR=/tmp \
   -e SDL_AUDIODRIVER=dummy \
   -e SDL_NO_MITSHM=1 -e SDL_VIDEO_X11_NO_SHM=1 \
