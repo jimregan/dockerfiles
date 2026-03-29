fa1e0122be0be4c8bcf84994019eeedc0e81a20b
Jim O'Regan <joregan@kth.se>
2026-03-29 22:17:37 +0000

    add

--- pylaia/get_data.sh
+++ pylaia/get_data.sh
@@ -1,11 +1,11 @@
 wget https://github.com/jimregan/tesseract-gle-uncial/releases/download/v0.1beta4_gen/seanchlo_generated.zip
 unzip seanchlo_generated.zip
-mkdir images
+mkdir data/images
 for set in train val test
 do
-	mkdir images/$set
+	mkdir data/images/$set
 	for i in $(cat data/$set.ids)
 	do
-		mv out/$i.jpg images/$set/
+		mv out/$i.jpg data/images/$set/
 	done
 done	
