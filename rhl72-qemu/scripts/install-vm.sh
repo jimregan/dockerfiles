6a9fb3c5b4c267160102402363b48d478a4a1621
Jim O'Regan <joregan@kth.se>
2026-05-06 19:35:24 +0000

    next

--- rhl72-qemu/scripts/install-vm.sh
+++ rhl72-qemu/scripts/install-vm.sh
@@ -8,7 +8,7 @@ DISC1=${DISC1:-/rhl72/isos/disc1.iso}
 DISC2=${DISC2:-/rhl72/isos/disc2.iso}
 DISK=${DISK:-/disk/rhl72.qcow2}
 INSTALL_BOOT=${INSTALL_BOOT:-direct}
-INSTALL_SCRIPT_REV=20260506-9
+INSTALL_SCRIPT_REV=20260506-10
 KS_ARG=${KS_ARG:-ks=http://10.0.2.2:8081/ks.cfg}
 
 echo "install-vm.sh revision: $INSTALL_SCRIPT_REV"
@@ -168,13 +168,28 @@ if [ -f "$MNT1/images/$BOOT_IMAGE" ]; then
     cp "$INITRD_IMG.gz" "$BOOTMNT/initrd.img"
     rm -rf "$INITRD_WORK"
 
+    ORIGINAL_APPEND=$(awk '
+        /^[[:space:]]*append[[:space:]]/ {
+            sub(/^[[:space:]]*append[[:space:]]+/, "")
+            print
+            exit
+        }
+    ' "$BOOTMNT/syslinux.cfg")
+    if [ -z "$ORIGINAL_APPEND" ]; then
+        ORIGINAL_APPEND="initrd=initrd.img"
+    fi
+    BASE_APPEND=$(printf '%s\n' "$ORIGINAL_APPEND" | sed -E 's/(^| )ks=[^ ]+//g; s/(^| )method=[^ ]+//g; s/(^| )ksdevice=[^ ]+//g; s/(^| )ip=[^ ]+//g; s/  +/ /g; s/^ //; s/ $//')
+
+    echo "Original boot floppy append args: $ORIGINAL_APPEND"
+    echo "Base boot floppy append args: $BASE_APPEND"
+
     cat > "$BOOTMNT/syslinux.cfg" << EOF
 default linux
 prompt 0
 timeout 1
 label linux
   kernel vmlinuz
-  append initrd=initrd.img $APPEND_ARGS
+  append $BASE_APPEND $APPEND_ARGS
 EOF
     cp /rhl72/kickstart.cfg "$BOOTMNT/ks.cfg"
     echo "Patched boot floppy syslinux.cfg:"
