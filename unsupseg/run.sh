4626365340d99c6adc5bc90f45b70a81c13918d0
Jim O'Regan <joregan@kth.se>
2026-08-11 23:08:12 +0000

    try again

--- unsupseg/run.sh
+++ unsupseg/run.sh
@@ -1,5 +1,6 @@
   docker run --rm --gpus all \
-    --ipc=host \
+    --shm-size=4g \
+    -e CUDA_LAUNCH_BLOCKING=1 \
     -v /home/joregan/waxholm-unsupseg:/data/waxholm-unsupseg:ro \
     -v /home/joregan/unsupseg-output:/output \
     jimregan/unsupseg \
@@ -7,5 +8,8 @@
       data=buckeye \
       buckeye_path=/data/waxholm-unsupseg \
       buckeye_percent=1.0 \
+      batch_size=1 \
+      dataloader_n_workers=0 \
+      gpus=1 \
       hydra.run.dir=/output/unsupseg-waxholm \
       exp_name=waxholm
